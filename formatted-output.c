d24273b64945c6065795f2506a8cd21052f6c233
Brandon Murry <brandonfossedu@fcboe.org>
2023-01-30 10:15:11 -0500

    Added new file

--- formatted-output.c
+++ formatted-output.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int i = 16;
-    float f = 3.141592;
+    int a = 15;
+    float x = 3.141592;
     char str[10];
 
-    printf("Integer representation: %i\n", i);
-    printf("Hexadecimal representation: %x\n", i);
+    // Output variables using different specifier characters
+    printf("Integer representation: %i\n", a);
+    printf("Hexadecimal representation: %x\n", a);
 
-    printf("Decimal floating point: %f\n", f);
-    printf("Hexadecimal floating point: %a\n", f);
+    printf("Decimal floating point: %f\n", x);
+    printf("Hexadecimal floating point: %a\n", x);
+
+    // Output i with different width fields
+    printf("Print i with width of 3 characters:%3i\n", a);
+    printf("Print i with width of 9 characters:%9i\n", a);
+
+    // Output f with different precisions
+    printf("Print f with 2 decimal precision:%.2f\n", x);
+    printf("Print f with 8 decimal precision:%.8f\n", x);
+
 
     return 0;
 }
